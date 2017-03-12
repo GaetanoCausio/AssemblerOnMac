1fa56d43caecbc2591059e089248a73c30a73460
Gaetano Causio <gaetano.causio@gmail.com>
2017-03-12 12:36:19 +0100

    initial version

---
+++ asm.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# ------------------------------------------------------------- #
+# asm.sh v1.1 (c) 2017 Gaetano Causio                           #
+#                                                               #
+# Script to compile, link and run an assembly program           #
+#                                                               #
+# please note that you may need to change option of 'ld'        #
+# from -macosx_version_min 10.12 to your current OSX version    # 
+# ------------------------------------------------------------- #
+
+# get command line parameters
+prmProgramName="$1"
+prmNorun="$2"
+prmCount=$#
+
+# set handy variables
+bold=$(tput bold)
+normal=$(tput sgr0)
+underline=$(tput smul) 
+baseName="${prmProgramName%.*}"
+
+echo "asm v1.1 (c) 2017 Gaetano Causio"
+echo
+
+# Check command line parameters
+# -----------------------------
+
+if [ $prmCount == 0 ] ; then # no input parameters
+   echo "Wrong number of parameters. Usage is:"
+   echo
+   echo "   asm ${underline}filename${normal} [norun] [parameters]"
+   echo
+   echo "   - filename   = name of your assemply code program"
+   echo "   - norun      = use <norun> if you just need to compile & link without running it"
+   echo "   - parameters = pass any parameters to your assembly program"   
+   echo
+   exit 0
+fi
+
+if [ ! -r "$prmProgramName" ]; then # assembly program not readable
+   echo "Unable to access file: ${bold}$prmProgramName${normal}"
+   echo
+   echo "Please specify a valid file name"
+   echo
+   exit 0
+fi
+
+# Compile and Link
+# ----------------
+
+echo "Compile, link and run for assemply code: ${bold}$prmProgramName${normal}"
+echo
+
+echo -n "Compiling... "
+as $prmProgramName -o $baseName.o -arch i386
+if [ $? == 0 ] ; then # if exit code is zero
+   echo "Ok"
+else
+   echo
+   exit 1   
+fi   
+
+echo -n "Linking... "
+ld $baseName.o -e _main -lc -macosx_version_min 10.12 -o $baseName -arch i386 -no_pie
+if [ $? == 0 ] ; then # if exit code is zero
+   echo "Ok"
+else
+   echo
+   exit 1   
+fi   
+
+# Execute Assembly program
+# ------------------------
+
+if [ "$prmNorun" != "norun" ] ; then
+   if [ $prmCount != 1 ] ; then # parameters for assembly program are prensent
+      echo "Running with parameters: $2 $3 $4 $5 $6 $7 $8 $9"
+   else
+      echo "Running:"
+   fi
+   echo
+   ./$baseName $2 $3 $4 $5 $6 $7 $8 $9
+fi
+
+echo
+echo "Completed. 👍 "
+
---
+++ hello.s
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------- #
+# hello.s v1.0 (c) 2017 Gaetano Causio                          #
+#                                                               #
+# Sample assembly program. It display a string on console by    #
+# calling the _printf subroutine.                               #
+# ------------------------------------------------------------- #
+
+	.globl	_main                   # set start point of program
+
+# program internal memory
+# -----------------------
+	
+message:                                 
+	.asciz	"Hello, Yes We Can! 🍀 \n"
+	
+# program starts here
+# -------------------
+
+_main:                             
+                             
+	push 	%ebp			        # save base-pointer register 
+	
+# display message on console
+# --------------------------
+
+    sub 	$8, %esp                # reserve bytes from the stack to call _printf
+		
+    lea     message, %eax           # get memory address of message string...	
+	mov 	%eax, (%esp)            # ...and store it into _printf parameters stack (%esp)
+	
+	call 	_printf                 # display a string on console
+	
+	add 	$8, %esp                # free up reserved stack memory
+
+# exit program
+# ------------
+
+	pop 	%ebp                    # restore base-pointer register 
+	
+	xor		%eax, %eax				# set return code to zero
+	ret								# exit
+
